179acd8f248b7ec09660c39dea8d84f66b7dbe52
javichu184 <104259747+javichu184@users.noreply.github.com>
2024-04-23 12:31:34 +0200

    Add files via upload

---
+++ Ejemplo1/Ejemplo1/main.cpp
@@ -0,0 +1,12 @@
+﻿#include <cstdio>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    printf("¡hola desde %s! \n", "Ejemplo1");
+
+    cout << "buenas tardes" << endl;
+    return 0;
+}
